ee9a8c78c7f68fdc54951b999517185a12ca08d5
Adam Gressen <newgrounds@users.noreply.github.com>
2017-10-04 10:26:40 -0400

    Add MONGODB_URI environment variable

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var env = process.env
 
 module.exports = {
   port: env.PORT || 3000,
-  mongoUrl: env.MONGOLAB_URI || env.MONGO_URL || 'mongodb://localhost/iou-slack-bot',
+  mongoUrl: env.MONGOLAB_URI || env.MONGODB_URI || env.MONGO_URL || 'mongodb://localhost/iou-slack-bot',
   tokens: (env.TOKEN || env.TOKENS) && (env.TOKEN || env.TOKENS).split(',') || []
 }
